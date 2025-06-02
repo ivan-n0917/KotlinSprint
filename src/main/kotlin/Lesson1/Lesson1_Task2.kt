f6655bdada2b275ab122d3d495b1b06774801e56
ivann <ivan.n0904@gmail.com>
2025-06-02 11:20:04 +0700

    KS-1-2 solution

---
+++ src/main/kotlin/Lesson1/Lesson1_Task2.kt
@@ -0,0 +1,16 @@
+package org.example.Lesson1
+
+fun main() {
+
+    val age: Int = 36
+    val thanksMessage: String = " Спасибо за покупку"
+
+    println(age)
+    println(thanksMessage)
+
+//  var employeeCount: Int = 2000
+//  println("Количество работников: $employeeCount")
+
+      var employeeCount = 1999
+    println("Обновленное количество работников: $employeeCount")
+}
