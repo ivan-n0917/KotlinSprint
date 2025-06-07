fbbfe59ab5807e50e90b642ac41958b1201184bc
ivann <ivan.n0904@gmail.com>
2025-06-07 10:06:50 +0700

    KS-1-5 refaktor

--- src/main/kotlin/Lesson1/Lesson1_Task5.kt
+++ src/main/kotlin/Lesson1/Lesson1_Task5.kt
@@ -1,15 +1,18 @@
 package org.example.Lesson1
 
-fun main() {
+const val SECONDS_IN_MINUTE = 60
+const val MINUTE_IN_HOUR = 60
+const val SECONDS_IN_HOUR = SECONDS_IN_MINUTE * MINUTE_IN_HOUR
 
-    val totalSeconds: Int = 6480
+fun duration(totalSeconds: Int): String {
+    val hours = totalSeconds / SECONDS_IN_HOUR
+    val secondsAfterHours = totalSeconds % SECONDS_IN_HOUR
+    val minutes = secondsAfterHours / SECONDS_IN_MINUTE
+    val secunds = secondsAfterHours % SECONDS_IN_MINUTE
 
-    val minutes = totalSeconds / 60
-    val remainingSeconds = totalSeconds % 60
-
-    val hours = minutes / 60
-    val remainingMinutes = minutes % 60
-
-    println("Время в космосе: %02d:%02d:%02d"
-        .format(hours, remainingMinutes, remainingSeconds))
+    return "02d:02d:02d".format(hours,minutes,secunds)
 }
+    fun main() {
+        fun totalSeconds = 6480
+        println("Время вкосмосе: ${duration(totalSeconds())}")
+    }
