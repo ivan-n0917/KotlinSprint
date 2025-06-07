4d130494cfd3e51ce34dce7e94da4246404fa71d
ivann <ivan.n0904@gmail.com>
2025-06-07 11:27:00 +0700

    KS-2-1 refactor

--- src/main/kotlin/Lesson2/Lesson2_task1.kt
+++ src/main/kotlin/Lesson2/Lesson2_task1.kt
@@ -2,6 +2,13 @@ package org.example.Lesson2
 
 fun main() {
 
-    val average = (3 + 4 + 3 + 5) / 4.0
-    println("Средний балл: %.2f".format(average))
+    val englishLanguageScores = listOf(3, 4, 3, 5)
+
+    val numberOfStudents = englishLanguageScores.size
+
+    val totalScoresSum = englishLanguageScores.sum().toFloat()
+
+    val averageScore = totalScoresSum / numberOfStudents
+
+    println("Средний балл по английскому языку: %.2f".format(averageScore))
 }
