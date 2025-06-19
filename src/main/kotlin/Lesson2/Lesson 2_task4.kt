470374e70469784af621a9a35cfe3388bc775bc4
ivann <ivan.n0904@gmail.com>
2025-06-19 14:26:03 +0700

    KS-2-4 solution

---
+++ src/main/kotlin/Lesson2/Lesson 2_task4.kt
@@ -0,0 +1,13 @@
+package org.example.Lesson2
+
+const val BASE_CRYSTAL = 7
+const val BASE_IRON = 11
+const val BUFF_PERCENT = 20
+
+fun main() {
+
+    fun calculateBonus(base: Int) = (base * BUFF_PERCENT / 100)
+
+    println("Бонусные кристаллические руды: ${calculateBonus(BASE_CRYSTAL)}")
+    println("Бонусные железные руды: ${calculateBonus(BASE_IRON)}")
+}
