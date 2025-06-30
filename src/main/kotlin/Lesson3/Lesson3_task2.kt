9bfc22f9b38258c930929310104030c17cac2870
Admin <ivan.n0917@gmail.com>
2025-06-30 22:47:28 +0700

    Ks-3-2-lines

---
+++ src/main/kotlin/Lesson3/Lesson3_task2.kt
@@ -0,0 +1,16 @@
+package org.example.Lesson3
+
+fun main() {
+
+    val surName = "Андреева"
+    val name = "Татьяна"
+    val patronymic =  "Сергеевна"
+    val age = "20 лет"
+
+    println("$surName $name $patronymic, $age")
+
+    val newSurName = "Сидорова"
+    val newAge = "22 года"
+
+    println("$newSurName $name $patronymic, $newAge")
+}
