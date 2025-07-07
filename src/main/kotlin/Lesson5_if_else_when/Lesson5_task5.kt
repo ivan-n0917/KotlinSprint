04258bf49883041fe6f2c6b589a94bbffef8ce39
Admin <ivan.n0917@gmail.com>
2025-07-07 17:04:58 +0700

    KS-5-4-conditional operators

---
+++ src/main/kotlin/Lesson5_if_else_when/Lesson5_task5.kt
@@ -0,0 +1,37 @@
+package org.example.Lesson5_if_else_when
+
+fun main() {
+
+    val winningNumbers = List(3) {(0..42).random() }
+    val userNumbers = listOf(1, 2, 3)
+
+    println("Добро пожаловать в лотерею!")
+    println("Угадайте три числа от 0 до 42 и выиграйте приз!")
+
+    print("Введите первое число: ")
+    readln().toInt()
+
+    print("Введите второе число: ")
+    readln().toInt()
+
+    print("Введите третье число: ")
+    readln().toInt()
+
+    val matchedNumbers = userNumbers.intersect(winningNumbers.toSet())
+    val matchCount = matchedNumbers.size
+
+    when (matchCount) {
+        3 -> println("Поздравляем! Вы угадали все три числа и выиграли ДЖЕКПОТ!")
+        2 -> println("Отличный результат! Вы угадали два числа и получаете крупный приз!")
+        1 -> println("Вы угадали одно число и получаете утешительный приз!")
+        else -> println("К сожалению, вы не угадали ни одного числа.")
+    }
+
+    println("Выигрышные числа были: $winningNumbers")
+
+    }
+
+
+
+
+
