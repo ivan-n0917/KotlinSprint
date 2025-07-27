0c7f688cff542e9b5bea66a41fee91507b18f624
Admin <ivan.n0917@gmail.com>
2025-07-27 14:55:44 +0700

    KS-7-1-For-Ranges

---
+++ src/main/kotlin/Lesson7_For_Ranges/Lesson7_Task1.kt
@@ -0,0 +1,14 @@
+package org.example.Lesson7_For_Ranges
+
+fun main() {
+    val password = buildString {
+        for (i in 0 .. 6) {
+            append(
+                if (i % 2 == 0) ('a'..'z').random()
+                else (0..9).random()
+            )
+        }
+    }
+
+    println("Сгенерированный пароль: $password")
+}
