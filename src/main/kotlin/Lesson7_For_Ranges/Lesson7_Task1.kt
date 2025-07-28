460b9e6d9186c3e58dcd426f01f99b72508ba654
Admin <ivan.n0917@gmail.com>
2025-07-28 20:51:17 +0700

    KS-7-5-For-Ranges-Refactoring

--- src/main/kotlin/Lesson7_For_Ranges/Lesson7_Task1.kt
+++ src/main/kotlin/Lesson7_For_Ranges/Lesson7_Task1.kt
@@ -2,7 +2,7 @@ package org.example.Lesson7_For_Ranges
 
 fun main() {
     val password = buildString {
-        for (i in 0 .. 6) {
+        for (i in 0 until  6) {
             append(
                 if (i % 2 == 0) ('a'..'z').random()
                 else (0..9).random()
