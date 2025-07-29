6e09401ef712223e41e0aca2e8bccfe3ae315fcd
Admin <ivan.n0917@gmail.com>
2025-07-29 08:43:20 +0700

    KS-7-4-For-Ranges-Refactoring

--- src/main/kotlin/Lesson7_For_Ranges/Lesson7_Task4.kt
+++ src/main/kotlin/Lesson7_For_Ranges/Lesson7_Task4.kt
@@ -4,13 +4,13 @@ fun main() {
     print("Введите количество секунд: ")
     val totalSeconds = readln().toIntOrNull()
 
-    println("Таймер запущен на $totalSeconds секунд")
-
     if (totalSeconds != null) {
+        println("Таймер запущен на $totalSeconds секунд")
+
         for (remainingSeconds in totalSeconds downTo 1) {
             println("Осталось: $remainingSeconds сек")
             Thread.sleep(1000)
         }
+        println("Время вышло!")
     }
-    println("Время вышло!")
 }
