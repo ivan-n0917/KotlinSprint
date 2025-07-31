81f716984b3b55939e417bcc6214b4d58668a28c
ivan-n0917 <ivan.n0904@gmail.com>
2025-07-31 08:17:02 +0700

    KS-8-4-Arrays-Refactoring

--- src/main/kotlin/Lesson8_Arrays/Lesson8_Task4.kt
+++ src/main/kotlin/Lesson8_Arrays/Lesson8_Task4.kt
@@ -2,19 +2,19 @@ package org.example.Lesson8_Arrays
 
 fun main() {
     val ingredients = mutableListOf("Мука", "Сахар", "Яйца", "Молоко", "Масло")
-
     println("Список ингредиентов: ${ingredients.joinToString()}")
 
     print("Какой ингредиент заменить? ")
     val oldItem = readln()
 
-    if (oldItem !in ingredients) {
+    val index = ingredients.indexOf(oldItem)
+    if (index == -1) {
         println("'$oldItem' нет в списке")
-        return
+
     }
 
     print("На что заменить? ")
-    ingredients[ingredients.indexOf(oldItem)] = readln()
+    ingredients[index] = readln()
 
     println("Готово! Новый список: ${ingredients.joinToString()}")
 }
