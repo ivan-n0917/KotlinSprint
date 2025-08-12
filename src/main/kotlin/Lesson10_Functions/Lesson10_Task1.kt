0f9d06471fcaf57512cc5cdf60e5d9750e990f7c
ivan-n0917 <ivan.n0904@gmail.com>
2025-08-12 15:07:38 +0700

    KS-10-5-Refactoring

--- src/main/kotlin/Lesson10_Functions/Lesson10_Task1.kt
+++ src/main/kotlin/Lesson10_Functions/Lesson10_Task1.kt
@@ -1,13 +1,16 @@
 package org.example.Lesson10_Functions
 
+fun rollDice(): Int {
+    return (1..6).random()
+}
+
 fun main() {
     println("Игра в кости: человек против компьютера")
 
-    val playerRoll = (1..6).random()
-    val computerRoll = (1..6).random()
+    val playerRoll = rollDice()
+    val computerRoll = rollDice()
 
     println("Игрок бросает кости... Выпало: $playerRoll")
-
     println("Компьютер бросает кости... Выпало: $computerRoll")
 
     when {
