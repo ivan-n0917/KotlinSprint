860e54fa953667e0ebf33667fe675350c028e3b2
ivan-n0917 <ivan.n0904@gmail.com>
2025-08-31 16:01:13 +0700

    KS-17-4-setters-and-getters-refactoring

--- src/main/kotlin/lesson17_setters_and_getters/task4.kt
+++ src/main/kotlin/lesson17_setters_and_getters/task4.kt
@@ -1,34 +1,36 @@
 package org.example.lesson17_setters_and_getters
 
 class Package(
-    val packageId: String,
+    private val packageId: String,
     private var location: String,
     private var moveCount: Int = 0,
 ) {
 
-    fun getLocation(): String = location
-
-    fun getMoveCount(): Int = moveCount
-
-    fun setLocation(newLocation: String) {
-        if (newLocation.isNotBlank() && newLocation != location) {
-            location = newLocation
-            moveCount++
-            println("Посылка $packageId прибыла в '$newLocation'. Перемещений: $moveCount")
-        } else {
-            println("Новое местоположение не может быть пустым или совпадать с текущим.")
+    var locationProperty: String = ""
+        get() = location
+        set(newLocation) {
+            if (newLocation.isNotBlank() && newLocation != field) {
+                field = newLocation
+                moveCount++
+                println("Посылка $packageId прибыла в '$newLocation'. Перемещений: $moveCount")
+            } else {
+                println("Новое местоположение не может быть пустым или совпадать с текущим.")
+            }
         }
-    }
+
+
+    val moveCountProperty: Int
+        get() = moveCount
 }
 
 fun main() {
     val package1 = Package("PKG-12345", "Москва")
 
-    println("Исходное местоположение: ${package1.getLocation()}")
-    println("Количество перемещений: ${package1.getMoveCount()}")
+    println("Исходное местоположение: ${package1.locationProperty}")
+    println("Количество перемещений: ${package1.moveCountProperty}")
 
-    package1.setLocation("Санкт-Петербург")
-    package1.setLocation("Екатеринбург")
-    package1.setLocation("Казань")
+    package1.locationProperty = "Санкт-Петербург"
+    package1.locationProperty = "Екатеринбург"
+    package1.locationProperty = "Казань"
 
 }
