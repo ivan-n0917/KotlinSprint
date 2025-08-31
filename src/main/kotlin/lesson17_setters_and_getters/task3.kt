fcb257d0e578a0c0ceaae9dbf6facfd9b31f9204
ivan-n0917 <ivan.n0904@gmail.com>
2025-08-31 15:54:14 +0700

    KS-17-3-setters-and-getters-refactoring

--- src/main/kotlin/lesson17_setters_and_getters/task3.kt
+++ src/main/kotlin/lesson17_setters_and_getters/task3.kt
@@ -3,27 +3,24 @@ package lesson17_setters_and_getters
 class Folder(
     private val name: String,
     private var fileCount: Int,
-    private val isSecret: Boolean,
+    private val isSecret: Boolean
 ) {
 
-    fun getName(): String {
-        return if (isSecret) "скрытая папка" else name
-    }
+    val nameProperty: String
+        get() = if (isSecret) "скрытая папка" else name
 
-    fun getFileCount(): Int {
-        return if (isSecret) 0 else fileCount
-    }
+    val fileCountProperty: Int
+        get() = if (isSecret) 0 else fileCount
 }
 
 fun main() {
-
     val secretFolder = Folder("Документы", 5, true)
 
-    println("Имя папки: ${secretFolder.getName()}")      // Ожидаем: "скрытая папка"
-    println("Количество файлов: ${secretFolder.getFileCount()}")  // Ожидаем: 0
+    println("Имя папки: ${secretFolder.nameProperty}")      // Ожидаем: "скрытая папка"
+    println("Количество файлов: ${secretFolder.fileCountProperty}")  // Ожидаем: 0
 
     val regularFolder = Folder("Фото", 20, false)
 
-    println("Имя папки: ${regularFolder.getName()}")     // Ожидаем: "Фото"
-    println("Количество файлов: ${regularFolder.getFileCount()}")  // Ожидаем: 20
+    println("Имя папки: ${regularFolder.nameProperty}")     // Ожидаем: "Фото"
+    println("Количество файлов: ${regularFolder.fileCountProperty}")  // Ожидаем: 20
 }
