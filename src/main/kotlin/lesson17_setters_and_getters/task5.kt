fac05ddf60f794ed8b8e625763bbf4aa3e1b9bb5
ivan-n0917 <ivan.n0904@gmail.com>
2025-08-31 16:09:56 +0700

    KS-17-5-setters-and-getters-refactoring

--- src/main/kotlin/lesson17_setters_and_getters/task5.kt
+++ src/main/kotlin/lesson17_setters_and_getters/task5.kt
@@ -4,34 +4,33 @@ class User {
     private var login: String? = null
     private var password: String? = null
 
-    fun getLogin(): String? = login
-
-    fun getPassword(): String {
-        return if (password.isNullOrBlank()) "" else "*".repeat(password!!.length)
-    }
-
-    fun setLogin(newLogin: String) {
-        if (newLogin.isNotBlank()) {
-            login = newLogin
-            println("Логин успешно изменён на: $newLogin")
-        } else {
-            println("Новый логин не может быть пустым.")
+    var loginProperty: String?
+        get() = login
+        set(value) {
+            if (!value.isNullOrBlank()) {
+                login = value
+                println("Логин успешно изменён на: $value")
+            } else {
+                println("Новый логин не может быть пустым.")
+            }
         }
-    }
 
-    fun setPassword() {
-        println("Вы не можете изменить пароль.")
-    }
+    val passwordProperty: String
+        get() = if (password.isNullOrBlank()) "" else "*".repeat(password!!.length)
+
+    var passwordPropertySetter: String
+        get() = passwordProperty
+        set(value) {
+            println("Вы не можете изменить пароль.")
+        }
 }
 
 fun main() {
     val user = User()
 
-    user.setLogin("ivan_ivanov")
-    user.setLogin("ivan_new")
+    user.loginProperty = "ivan_ivanov"
+    user.loginProperty = "ivan_new"
 
-    user.setPassword()
-
-    println("Логин: ${user.getLogin()}")
-    println("Пароль: ${user.getPassword()}")
+    println("Логин: ${user.loginProperty}")
+    println("Пароль: ${user.passwordProperty}")
 }
