10fe3d0b29c6e460326d1c152d5562e4fb369456
ivan-n0917 <ivan.n0904@gmail.com>
2025-09-06 12:50:59 +0700

    KS-17-4-setters-and-getters

---
+++ src/main/kotlin/lesson17_setter_and_getters/lesson17_task4.kt
@@ -0,0 +1,20 @@
+package org.example.lesson17_setter_and_getters
+
+class Order(val number: String, location: String) {
+    var location = location
+        set(value) {
+            field = value
+            moveCount++
+        }
+    private var moveCount = 0
+    val moves get() = moveCount
+}
+
+fun main() {
+    val parcel = Order("12345", "Склад отправителя")
+
+    listOf("Сортировочный центр", "Город назначения", "Пункт выдачи").forEach {
+        parcel.location = it
+        println("Посылка ${parcel.number} в ${parcel.location}, перемещений: ${parcel.moves}")
+    }
+}
