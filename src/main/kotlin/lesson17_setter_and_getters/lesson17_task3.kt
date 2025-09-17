1e908c35d21afa104cfd7acfeedf6c9daadb7403
ivan-n0917 <ivan.n0904@gmail.com>
2025-09-17 19:19:49 +0700

    KS-17-2-setters-and-getters-refactoring2

--- src/main/kotlin/lesson17_setter_and_getters/lesson17_task3.kt
+++ src/main/kotlin/lesson17_setter_and_getters/lesson17_task3.kt
@@ -1,30 +1,32 @@
 package org.example.lesson17_setter_and_getters
 
 class Folder(
-    val name: String,
-    private var count: Int,
-    private val secret: Boolean = false,
+    name: String,
+    count: Int,
+    private val isSecret: Boolean,
 ) {
 
-    val shownName: String
-        get() = if (secret) "скрытая папка: количество файлов 0" else name
+    val originalName = name
+        get() = if (isSecret) "скрытая папка" else field
 
-    val shownCount: Int
-        get() = if (secret) 0 else count
+    val originalCount = count
 }
 
 fun main() {
     print("Введите номер папки: ")
     val name = readln()
 
-    val folder = Folder(name, 8)
+    val folder = Folder(
+        name, 8,
+        isSecret = true
+    )
 
     print("\nВыберите действие:\n1 - Показать статус\n2 - Показать количество файлов\nВаш выбор: ")
     val choice = readln().trim()
 
     when (choice) {
-        "1" -> println(folder.shownName)
-        "2" -> println("Файлов: ${folder.shownCount}")
+        "1" -> println(folder.originalName)
+        "2" -> println("Файлов: ${folder.originalCount}")
         else -> println("Неверный выбор.")
     }
 }
